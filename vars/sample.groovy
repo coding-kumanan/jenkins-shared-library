aa0935f0613ecf183ad7f321214ba68f0f2259c8
kumanan <kumananboss@gmail.com>
2024-02-08 14:59:40 +0530

    auto upload

--- vars/sample.groovy
+++ vars/sample.groovy
@@ -1,7 +1,6 @@
 //declaring function with arguments 
 def info(message) {
-    echo "INFO from inside the function"
     echo "Info from trainer is $message"
 }
 //calling the function
-info()
+info(hello)
